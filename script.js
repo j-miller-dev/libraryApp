592c1b5ec74a86e622fb59ec5bc38d42b2e5927d
Jason Miller <jasonmiller.dev@gmail.com>
2022-10-08 00:11:11 +1100

    Second commit, initialized some data structures

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
+// DATA STRUCTURES
 
-
-function Book() {
+// Book constructor
+class Book {
   // the constructor...
-  constructor() {
-      this.title = title;
-      this.author = author;
-      this.pageCount = pageCount;
-      this.read = read;
+  constructor(
+    title = "No Title",
+    author = "No Author",
+    pageCount = "0",
+    read = false
+  ) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.read = read;
   }
 }
+
+// Library constructor
+class Library {
+  constructor() {
+    this.books = [];
+  }
+  // add a book to the library
+  addBook() {}
+  // remove a book from the library
+  removeBook() {}
+}
+
+const library = new Library();
 
 let myLibrary = [
   (book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)),
   (book2 = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, true)),
-  (book3 = new Book("The Two Towers", "J.R.R. Tolkien", 352, true)),
-  (book4 = new Book("The Return of the King", "J.R.R. Tolkien", 416, true)),
-  (book5 = new Book("The Silmarillion", "J.R.R. Tolkien", 412, true)),
 ];
 
 function addBookToLibrary() {
